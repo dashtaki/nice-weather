b250ad4c095647438c86c66a191d384a6d2ddb76
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-22 23:05:12 +0200

    Init cities-list.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {CitiesListComponent} from './components/cities-list/cities-list.component';
 
-import { AppComponent } from './app.component';
+const DECLARATIONS = [
+    AppComponent,
+    CitiesListComponent
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [...DECLARATIONS],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
