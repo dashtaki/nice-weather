ccb5ae54d80c50f5610b24fe745b1f2165ae4e2a
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-27 22:48:40 +0200

    Write test for api.interceptor

--- src/app/interceptors/api.interceptor.spec.ts
+++ src/app/interceptors/api.interceptor.spec.ts
@@ -1,15 +1,61 @@
-import {TestBed} from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 import {ApiInterceptor} from './api.interceptor';
+import {Router} from '@angular/router';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController, TestRequest} from '@angular/common/http/testing';
+import {TransportService} from '../../services/transport.service';
+import {API_KEY} from '../../constants/ApiConstants';
 
 describe('ApiInterceptor', () => {
-    beforeEach(() => TestBed.configureTestingModule({
-        providers: [
-            ApiInterceptor
-        ]
-    }));
+    let mockRouter;
+    let mockTransportService;
+    let httpMock: HttpTestingController;
+    let transportService: TransportService;
+
+    function produceApiError(status: number): void {
+        const mockErrorResponse: any = {status, statusText: 'Bad Request'};
+        const data: string = 'Invalid';
+        transportService.Read('sad').subscribe();
+        const expectedQuery: string = `sad&appid=${API_KEY}`;
+        const req: TestRequest = httpMock.expectOne(expectedQuery);
+        req.flush(data, mockErrorResponse);
+    }
+
+    beforeEach(() => {
+        mockRouter = jasmine.createSpyObj(['navigateByUrl']);
+        mockTransportService = jasmine.createSpyObj(['Read']);
+
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                {
+                    provide: HTTP_INTERCEPTORS,
+                    useClass: ApiInterceptor,
+                    multi: true,
+                },
+                ApiInterceptor,
+                {provide: Router, useValue: mockRouter}
+            ]
+        });
+
+        httpMock = TestBed.inject(HttpTestingController);
+        transportService = TestBed.inject(TransportService);
+    });
 
     it('should be created', () => {
         const interceptor: ApiInterceptor = TestBed.inject(ApiInterceptor);
         expect(interceptor).toBeTruthy();
     });
+
+    it('should route to not found page if API status is 400', fakeAsync(() => {
+        produceApiError(400);
+
+        expect(mockRouter.navigateByUrl).toHaveBeenCalledWith('/not-found');
+    }));
+
+    it('should route to unauthorized page if API status is 401', fakeAsync(() => {
+        produceApiError(401);
+
+        expect(mockRouter.navigateByUrl).toHaveBeenCalledWith('/unauthorized');
+    }));
 });
--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable, throwError} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Router} from '@angular/router';
 import {catchError} from 'rxjs/operators';
+import {SpinnerService} from '../../services/spinner/spinner.service';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
 
-    constructor(private router: Router) {
+    constructor(private router: Router,
+                private spinnerService: SpinnerService) {
     }
 
     private handleAuthError(err: HttpErrorResponse): Observable<any> {
@@ -17,15 +19,14 @@ export class ApiInterceptor implements HttpInterceptor {
         if (err.status === 400) {
             this.router.navigateByUrl(`/not-found`);
         }
-        return throwError(err);
+        this.spinnerService.loading = false;
+        return of(err.message);
     }
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
         const authReq = request.clone();
         return next.handle(authReq)
-            .pipe(
-                catchError(error => this.handleAuthError(error))
-            );
+            .pipe(catchError(error => this.handleAuthError(error)));
     }
 
 }
