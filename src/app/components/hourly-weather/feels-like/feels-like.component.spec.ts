f38d3fffdd84728660d97caaa1d99aeda56610cf
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-27 14:20:10 +0200

    Write test for feels-like.component

--- src/app/components/hourly-weather/feels-like/feels-like.component.spec.ts
+++ src/app/components/hourly-weather/feels-like/feels-like.component.spec.ts
@@ -1,25 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FeelsLikeComponent } from './feels-like.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {FeelsLikeComponent} from './feels-like.component';
+import {CUSTOM_ELEMENTS_SCHEMA, DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('FeelsLikeComponent', () => {
-  let component: FeelsLikeComponent;
-  let fixture: ComponentFixture<FeelsLikeComponent>;
+    let component: FeelsLikeComponent;
+    let fixture: ComponentFixture<FeelsLikeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FeelsLikeComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FeelsLikeComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FeelsLikeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FeelsLikeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should show feels kile icon', () => {
+        const icon: DebugElement = fixture.debugElement.query(By.css('fa-icon'));
+
+        expect(icon.nativeElement.icon.iconName).toEqual('thermometer-half');
+    });
+
+    it('should show feels like weather', () => {
+        component.feelsLike = 12;
+        const feelsLike: DebugElement = fixture.debugElement.query(By.css('span'));
+        fixture.detectChanges();
+
+        expect(feelsLike.nativeElement.textContent).toContain('12 °C');
+    });
 });
