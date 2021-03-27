d62358214c39fc0204bd8e5315ec8c540def073f
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-27 14:56:37 +0200

    Write test for temperature.component

--- src/app/components/hourly-forecast/temperature/temperature.component.spec.ts
+++ src/app/components/hourly-forecast/temperature/temperature.component.spec.ts
@@ -1,25 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TemperatureComponent } from './temperature.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TemperatureComponent} from './temperature.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('TemperatureComponent', () => {
-  let component: TemperatureComponent;
-  let fixture: ComponentFixture<TemperatureComponent>;
+    let component: TemperatureComponent;
+    let fixture: ComponentFixture<TemperatureComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TemperatureComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TemperatureComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TemperatureComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TemperatureComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+    it('should show temperature', () => {
+        component.temperature = 18;
+        fixture.detectChanges();
+        const temperature: DebugElement = fixture.debugElement.query(By.css('span'));
+
+        expect(temperature.nativeElement.textContent).toContain('18');
+    });
+
+    it('should show degree symbol', () => {
+        const degreeSymbol: DebugElement = fixture.debugElement.queryAll(By.css('span'))[1];
+
+        expect(degreeSymbol.nativeElement.textContent).toContain('°C');
+    });
 });
