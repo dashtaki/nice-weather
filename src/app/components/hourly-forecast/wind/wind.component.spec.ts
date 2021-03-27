a6760d3045019c4815d3abcf4f6984bc25c86726
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-27 15:07:11 +0200

    Write test for wind.component

--- src/app/components/hourly-forecast/wind/wind.component.spec.ts
+++ src/app/components/hourly-forecast/wind/wind.component.spec.ts
@@ -1,6 +1,7 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {WindComponent} from './wind.component';
-import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('WindComponent', () => {
     let component: WindComponent;
@@ -22,5 +23,27 @@ describe('WindComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should show feels kile icon', () => {
+        const icon: DebugElement = fixture.debugElement.query(By.css('fa-icon'));
+
+        expect(icon.nativeElement.icon.iconName).toEqual('wind');
+    });
+
+    it('should show wind speed', () => {
+        component.windSpeed = 78;
+        fixture.detectChanges();
+        const windSpeed: DebugElement = fixture.debugElement.query(By.css('span'));
+
+        expect(windSpeed.nativeElement.textContent).toContain('78 m/s');
+    });
+
+    it('should show wind degrre', () => {
+        component.windDegree = 12;
+        fixture.detectChanges();
+        const windDegree: DebugElement = fixture.debugElement.queryAll(By.css('span'))[1];
+
+        expect(windDegree.nativeElement.textContent).toContain('12 °');
     });
 });
