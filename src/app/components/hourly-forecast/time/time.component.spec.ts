52da4fd443a4061d3b195559eb0c192b90984279
Mohammad Jamal Dashtaki <mjd.prg@gmail.com>
2021-03-27 14:59:29 +0200

    Write test for time.component

--- src/app/components/hourly-forecast/time/time.component.spec.ts
+++ src/app/components/hourly-forecast/time/time.component.spec.ts
@@ -1,25 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TimeComponent } from './time.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TimeComponent} from './time.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('TimeComponent', () => {
-  let component: TimeComponent;
-  let fixture: ComponentFixture<TimeComponent>;
+    let component: TimeComponent;
+    let fixture: ComponentFixture<TimeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TimeComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TimeComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TimeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TimeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should show date time with right format', () => {
+        component.uniTime = 1616846400;
+        fixture.detectChanges();
+        const time: DebugElement = fixture.debugElement.query(By.css('span'));
+
+        expect(time.nativeElement.textContent).toContain('2:00 PM');
+    });
 });
